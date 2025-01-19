31189b54641299d759e3c4212f5c6577ef8ce094
Kasiet21 <kasiet112103@gmail.com>
2025-01-19 22:54:58 +0700

    “copy”

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/cloud', function () { return view('cloud'); });
+Route::get('/cloud', function () {
+     return view('cloud');
+    
+});
+Route::get('/copy', function () {
+    return view('welcome2');
+   
+});
